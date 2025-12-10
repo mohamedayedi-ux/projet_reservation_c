90553329f0d93190c9e7bdbc22b767ee585d9bbc
mohamedayedi-ux <mohamed.ayedi@ensi-uma.tn>
2025-12-10 18:35:52 +0100

    Fix header guard and formatting in Menu.h

--- Menu.h
+++ Menu.h
@@ -4,17 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Structures.h"  
+#include "Structures.h" 
 #include "GestionSalle.h"
 #include "Reservation.h"
 #include "Stats.h"
 #include "FonctionsAux.h"
 #include "Facture.h"
 
+
 extern Salle salles[MAX_SALLES];
 extern int nb_salles;
 extern Reservation reservations[MAX_RES];
 extern int nb_reservations;
+
 
 void menu();
 
