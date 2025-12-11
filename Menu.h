fddac1be5d0cd042748a14b2436cc4aa8b5656da
MedLouaiDarguech <mohamedlouai.darguech@ensi-uma.tn>
2025-12-11 02:35:56 +0100

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -4,20 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Structures.h" 
+#include "Structures.h"  // <-- indispensable !
 #include "GestionSalle.h"
 #include "Reservation.h"
 #include "Stats.h"
 #include "FonctionsAux.h"
 #include "Facture.h"
 
-
+// Variables globales (déclarées dans main.c)
 extern Salle salles[MAX_SALLES];
 extern int nb_salles;
 extern Reservation reservations[MAX_RES];
 extern int nb_reservations;
 
-
+// Fonction principale du menu
 void menu();
 
-#endif 
+#endif  // MENU_H
